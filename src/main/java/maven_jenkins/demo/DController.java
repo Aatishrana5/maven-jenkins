2fbc2e27907420a787599a14d38baa59a5ee0ede
Aatish Rana <aatishrana315@gmail.com>
2026-04-15 10:20:39 +0530

    connect maven with jenkins

---
+++ src/main/java/maven_jenkins/demo/DController.java
@@ -0,0 +1,19 @@
+package maven_jenkins.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DController {
+
+	@RequestMapping("/test")
+	public String getMessage() 
+	{
+		return "Welcome to Jenkins with Spring Boot!";
+	}
+	 @RequestMapping("/details")
+	 public String getDetails() 
+	 {
+		 return "This is a sample application to demonstrate Jenkins integration with Spring Boot.";
+	 }
+	}
